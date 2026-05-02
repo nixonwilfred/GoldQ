0e48d550e613bce32b556034246bd13a2f3f5ef9
Nixon <nixonwilfred@gmail.com>
2026-05-02 22:42:41 +0530

    Remove outputFileTracingIncludes

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@
 const nextConfig = {
   images: {
     remotePatterns: []
-  },
-  outputFileTracingIncludes: {
-    "/analysis/[slug]": ["./content/analysis/**/*.mdx"]
   }
 };
 export default nextConfig;
